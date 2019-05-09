99620864ab03fbb62620734d401d88512bdddb95
Jean Santos <jean.santos@zup.com.br>
2019-05-09 17:54:20 -0300

    Initial Commit: Start project Ubuntu Connection
    Started projet Ubuntu Connection. Created first view choose Login/Create Profile/ Search.

---
+++ conexaoUbuntu/conexaoUbuntu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  conexaoUbuntu
+//
+//  Created by Jean Carlos Antonio Pereira dos Santos on 09/05/19.
+//  Copyright © 2019 Jean Carlos Antonio Pereira dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
