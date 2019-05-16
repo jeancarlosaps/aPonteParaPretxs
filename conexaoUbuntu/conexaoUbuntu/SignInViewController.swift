0d53909207e8fb2cc83b327a950892a018b5236d
Jean Santos <jean.santos@zup.com.br>
2019-05-16 13:14:55 -0300

    Configure View Login
    * Created outlets to manipulate;
    * Created Navigation to go back initial view;
    * Created and custom button forgot password;

---
+++ conexaoUbuntu/conexaoUbuntu/SignInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignInViewController.swift
+//  conexaoUbuntu
+//
+//  Created by Jean Carlos Antonio Pereira dos Santos on 15/05/19.
+//  Copyright © 2019 Jean Carlos Antonio Pereira dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+  
+  // MARK: - Outlets
+  @IBOutlet weak var tfMail: UITextField!
+  @IBOutlet weak var tfPassword: UITextField!
+  @IBOutlet weak var btnSignIn: UIButton!
+  @IBOutlet weak var btnForgotPassword: UIButton!
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
